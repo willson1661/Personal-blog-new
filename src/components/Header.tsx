635716ac5edfe50e98d5878039fd4e8b06bcde9d
willson <93517421+willson1661@users.noreply.github.com>
2025-07-09 00:40:50 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -45,7 +45,7 @@ const Header: React.FC<HeaderProps> = ({ darkMode, toggleDarkMode }) => {
       }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center py-3">
+        <div className="flex justify-between items-center py-1">
           {/* Logo */}
           <div className="flex-shrink-0">
           <img
