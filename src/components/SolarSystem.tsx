f99cd1f133e85c78446994e0ee907b5a88e69674
willson <93517421+willson1661@users.noreply.github.com>
2025-07-25 17:04:16 +0530

    Update SolarSystem.tsx

--- src/components/SolarSystem.tsx
+++ src/components/SolarSystem.tsx
@@ -7,53 +7,54 @@ interface SolarSystemProps {
 const SolarSystem: React.FC<SolarSystemProps> = ({ darkMode }) => {
   const solarSystemStyles = `
   .solar-container {
-    /* Use relative sizing and center the container */
-    font-size: calc(4px + 0.6vw);
-    width: 100%;
-    max-width: 400px;
-    max-height: 400px;
-    aspect-ratio: 1 / 1; /* Keep square shape */
-    position: relative;
-    margin: 0 auto;
+      font-size: calc(6px + 0.5vw); /* dynamic font sizing */
+      width: 100%;
+      max-width: 400px;
+      aspect-ratio: 1 / 1;
+      position: relative;
+      margin: 0 auto;
   }
 
   .sun {
-    position: absolute;
-    /* Use % positioning instead of fixed em */
-    top: 40%;
-    left: 50%;
-    width: 25%;
-    height: 25%;
-    background-color: ${darkMode ? "#2e2e2c" : "#000000"};
-    border-radius: 50%;
-    box-shadow: 0 0 0.6em ${darkMode ? "white" : "#000000"};
-    transform: translate(-50%, -50%);
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      width: 30%;
+      height: 30%;
+      background-color: ${darkMode ? "#2e2e2c" : "#000000"};
+      border-radius: 50%;
+      box-shadow: 0 0 1.5em ${darkMode ? "white" : "#000000"};
+      transform: translate(-50%, -50%);
   }
 
   .earth, .moon {
     position: absolute;
     border-style: solid;
-    border-color: ${darkMode ? "white transparent transparent transparent" : "#000000 transparent transparent transparent"};
-    border-width: 0.15em 0.15em 0 0;
+    border-color: ${
+      darkMode
+        ? "white transparent transparent transparent"
+        : "#000000 transparent transparent transparent"
+    };
+    border-width: 0.1em 0.1em 0 0;
     border-radius: 50%;
   }
 
   .earth {
-    top: 10%;
-    left: 10%;
-    width: 60%;
-    height: 60%;
-    animation: orbit 36.5s linear infinite;
-    transform-origin: 50% 50%;
+      top: 15%;
+      left: 15%;
+      width: 50%;
+      height: 50%;
+      animation: orbit 36.5s linear infinite;
+      transform-origin: 50% 50%;
   }
 
   .moon {
-    top: 60%;
-    right: 5%;
-    width: 15%;
-    height: 15%;
-    animation: orbit 2.7s linear infinite;
-    transform-origin: 50% 50%;
+      top: 60%;
+      right: 15%;
+      width: 15%;
+      height: 15%;
+      animation: orbit 2.7s linear infinite;
+      transform-origin: 50% 50%;
   }
 
   .earth::before,
@@ -64,22 +65,21 @@ const SolarSystem: React.FC<SolarSystemProps> = ({ darkMode }) => {
   }
 
   .earth::before {
-    /* Position relative to earth's size */
-    top: 18%;
-    right: 14%;
-    width: 10%;
-    height: 10%;
-    background-color: ${darkMode ? "#fffdfd" : "#000000"};
-    box-shadow: 0 0 0.7em ${darkMode ? "#e7e7e7" : "#000000"};
+      top: 14%;
+      right: 10%;
+      width: 8%;
+      height: 8%;
+      background-color: ${darkMode ? "#fffdfd" : "#000000"};
+      box-shadow: 0 0 1.5em ${darkMode ? "#e7e7e7" : "#000000"};
   }
 
   .moon::before {
-    top: 5%;
-    right: 20%;
-    width: 5%;
-    height: 5%;
-    background-color: ${darkMode ? "silver" : "#b7b7b7"};
-    box-shadow: 0 0 0.7em ${darkMode ? "#e7e7e7" : "#acacac"};
+      top: 3%;
+      right: 25%;
+      width: 3%;
+      height: 3%;
+      background-color: ${darkMode ? "silver" : "#000000"};
+      box-shadow: 0 0 1.5em ${darkMode ? "#e7e7e7" : "#000000"};
   }
 
   @keyframes orbit {
@@ -88,33 +88,39 @@ const SolarSystem: React.FC<SolarSystemProps> = ({ darkMode }) => {
     }
   }
 
-  /* Media Queries for smaller screens */
-  @media (max-width: 640px) {
+  /* Smaller screen adjustments */
+  @media (max-width: 480px) {
     .solar-container {
       max-width: 280px;
-      max-height: 280px;
-    }
-  }
-
-  @media (max-width: 400px) {
-    .solar-container {
-      max-width: 180px;
-      max-height: 180px;
-      font-size: calc(3px + 1vw);
+      font-size: calc(5px + 1vw);
     }
 
     .earth {
-      width: 70%;
-      height: 70%;
-      top: 15%;
-      left: 15%;
+      top: 20%;
+      left: 20%;
+      width: 55%;
+      height: 55%;
     }
 
     .moon {
+      top: 65%;
+      right: 12%;
       width: 18%;
       height: 18%;
-      top: 65%;
-      right: 4%;
+    }
+
+    .earth::before {
+      top: 16%;
+      right: 12%;
+      width: 10%;
+      height: 10%;
+    }
+
+    .moon::before {
+      top: 5%;
+      right: 28%;
+      width: 4%;
+      height: 4%;
     }
   }
   `;
